7bfeb17933c146436edcb9f0e70493d748b5026a
garudacrafts <derek@garudacrafts.com>
2016-10-20 13:45:06 -0400

    added React class to moviedb

--- src/components/moviedb.js
+++ src/components/moviedb.js
@@ -1,14 +1,46 @@
+const React = require('react')
 const h = require('react-hyperscript')
 const {
   Link
 } = require('react-router')
 
-const Moviedb = props => h('div', [
-  h('h1', 'Movies'),
-  h(Link, {
-    to: '/',
-    className: 'link'
-  }, 'Home')
-])
+const Moviedb = React.createClass({
+  getInitialState: function () {
+    return {
+      s: '',
+      r: 'json'
+    }
+  },
+  handleChange: function (e) {
+    this.setState({
+      s: e.target.value
+    })
+  },
+  handleSubmit: function (e) {
+    e.preventDefault()
+    this.setState({
+      
+    })
+  },
+  render: function () {
+    console.log(this.state)
+    return h('div.pv2.ph5', [
+      h('h1', 'Movies'),
+      h('form', {
+        onSubmit: this.handleSubmit
+      }, [
+        h('input.mb2', {
+          name: 's',
+          onChange: this.handleChange
+        })
+      ]),
+      h('button', 'Browse All'),
+      h(Link, {
+        to: '/',
+        className: 'link db mt2'
+      }, 'Home')
+    ])
+  }
+})
 
 module.exports = Moviedb
