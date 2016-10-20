f8990af13ff8b1c8d5e96f383fa32df46f5bb22f
chriscortes4u <chriscortes4u@yahoo.com>
2016-10-20 17:04:59 -0400

    edit movie img

--- src/components/moviedb.js
+++ src/components/moviedb.js
@@ -3,6 +3,47 @@ const h = require('react-hyperscript')
 const xhr = require('xhr')
 const { Link } = require('react-router')
 
+const newmanImg = './img/magic-word-2.gif'
+
+function renderMovie(movie){
+var movieImg = (movie.Poster === 'N/A') ? newmanImg : movie.Poster
+  return(
+    h('div.movie-container', [
+      h('div.fl.db.w-100.w-50-m.w-25-l.overflow-hidden', {
+        style: {
+          position: 'relative',
+          cursor: 'pointer'
+        }
+      }, [
+        h('a.db.w-100.h-100.aspect-ratio--4x6.grow', {
+          href: 'http://www.imdb.com/title/' + movie.imdbID,
+          target: '_blank',
+          style: {
+            background: `url(${movieImg}) no-repeat center center`,
+            backgroundSize: 'cover'
+          }
+        }, ''),
+        h('h2.f3.pa2.tc', {
+          style: {
+            position: 'absolute',
+            bottom: '10px',
+            left: '0'
+          }
+        }, [
+          h('a.link', {
+            href: 'http://www.imdb.com/title/' + movie.imdbID,
+            style: {
+              color: 'white',
+              textShadow: '2px 2px 6px rgba(0,0,0,.9)'
+            }
+          },
+          movie.Title + ' (' + movie.Year + ')')
+        ]
+        )
+      ])
+    ])
+  )
+}
 const Moviedb = React.createClass({
   getInitialState: function () {
     return {
@@ -36,69 +77,33 @@ const Moviedb = React.createClass({
 
     if (this.state.results['Response'] === 'False') {
       content = [
-        h('p.f1', { 'style': {'color': 'red'} }, 'No Results'),
-        h('img', { src: './img/magic-word-2.gif' })
+        h('p.f1.tc', { 'style': {'color': 'red'} }, 'No Results'),
+        h('img.', { src: newmanImg })
       ]
     }
     if (this.state.results['Search']) {
-      // count = this.state.results['Search'].length
-      content = this.state.results['Search'].map(movie =>
-        h('div.movie-container', [
-          h('div.fl.db.w-100.w-50-m.w-25-l.overflow-hidden', {
-            style: {
-              position: 'relative',
-              cursor: 'pointer'
-            }
-          }, [
-            h('a.db.w-100.h-100.aspect-ratio--4x6.grow', {
-              href: 'http://www.imdb.com/title/' + movie.imdbID,
-              target: '_blank',
-              style: {
-                background: `url(${movie.Poster}) no-repeat center center`,
-                backgroundSize: 'cover'
-              }
-            }, ''),
-            h('h2.f3.pa2.tc', {
-              style: {
-                position: 'absolute',
-                bottom: '10px',
-                left: '0'
-              }
-            }, [
-              h('a.link', {
-                href: 'http://www.imdb.com/title/' + movie.imdbID,
-                style: {
-                  color: 'white',
-                  textShadow: '2px 2px 6px rgba(0,0,0,.9)'
-                }
-              },
-              movie.Title + ' (' + movie.Year + ')')
-            ]
-            )
-          ])
-        ])
-      )
+      content = this.state.results['Search'].map(movie =>renderMovie(movie))
     }
 
-    return h('div.pv2.ph5', [
+    return h('div.pv2.ph5.bg-mid-gray', [
       h('header', [
-        h('h1', 'Movies')
+        h('h1.tc', 'Movie Search')
         // h('h2', count)
       ]),
-      h('form', {
+      h('form.tc', {
         onSubmit: this.handleSubmit
       }, [
         h('input.mb2', {
           onChange: this.handleChange
         }),
         h('p', [
-          h('button', 'Browse All')
+          h('button.tc', 'Browse All')
         ])
       ]),
       h('div.content.cb', content),
       h(Link, {
         to: '/',
-        className: 'link db mt2'
+        className: 'link db mv4 tc cl'
       }, 'Home')
     ])
   }
